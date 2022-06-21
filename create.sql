9e4bd7dc8807287e3a38d4a894d23ceb2cf05b20
MissDine <enidnyatichi1212@gmail.com>
2022-06-21 09:42:52 +0300

    Feature edit

---
+++ create.sql
@@ -0,0 +1,5 @@
+CREATE TABLE users{
+    id INTERGER PRIMARY KEY,
+    fullname TEXT,
+    username TEXT,
+    age INTERGER};
---
+++ insert.sql
@@ -0,0 +1,3 @@
+INSERT INTO Users (fullname, username, age);
+INSERT INTO Users (fullname, username, age);
+INSERT INTO Users (fullname, username, age);
